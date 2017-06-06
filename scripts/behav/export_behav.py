2759c2402b6092ada506db1d0112a80006f8448e
fliem <franz.liem@gmail.com>
2017-06-06 17:36:46 +0200

    ud for new structure

--- scripts/behav/export_behav.py
+++ scripts/behav/export_behav.py
@@ -1,15 +1,46 @@
 import os, pandas as pd
 from glob import glob
 from lhab_pipelines.nii_conversion.utils import get_public_sub_id
+import numpy as np
 
 
-def export_behav_with_new_id(orig_file, s_id_lut, out_dir):
-    df_orig = pd.read_excel(orig_file, na_values=["NA01", "NA02", "NA03", "NA04"])
-    #fixme remove na counts in last line
+def long_to_wide(long, index, columns, values, nonumeric=False):
+    if nonumeric:
+        wide = long.pivot_table(index=index, columns=columns, values=values, aggfunc=lambda x: ', '.join(x.unique()))
+    else:
+        wide = long.pivot_table(index=index, columns=columns, values=values)
+    l = wide.columns.labels
+    le = wide.columns.levels
+    o = []
+    out_cols = []
+
+    for i, ii in enumerate(l):
+        o.append(le[i][l[i]])
+    o = np.array(o)
+
+    for i in o.T:
+        out_cols.append("_".join(i))
+    out_cols = [s.replace("value_", "") for s in out_cols]
+    out_cols = [s.replace("z_", "") + "_z" if s.startswith("z_") else s for s in out_cols]
+
+    wide.columns = out_cols
+    wide.reset_index(inplace=True)
+    return wide
+
+
+def export_behav_with_new_id(orig_file, s_id_lut):
+    df_orig = pd.read_excel(orig_file, na_values=["NA01", "NA02", "NA03", "NA04", "NA4", "X", 888, 999])
+    # na_values seems not to catch numbers consistently
+    df_orig.replace({888: np.nan, 999: np.nan}, inplace=True)
+
+    # fixme remove na counts in last line
     df_orig.dropna(subset=["vp_code"], inplace=True)
     df_orig.rename(columns={"vp_code": "private_subject_id"}, inplace=True)
-    df_orig["private_subject_id"].replace({"vcp4 ": "vcp4"}, inplace=True)
-    score_cols = df_orig.columns.drop(['private_subject_id', 'tp_avail'])
+    # df_orig["private_subject_id"].replace({"vcp4 ": "vcp4"}, inplace=True)
+    df_orig["private_subject_id"] = df_orig["private_subject_id"].str.strip()
+    score_cols = df_orig.columns.drop(['private_subject_id'])
+    if "tp_avail" in score_cols:
+        score_cols = score_cols.drop("tp_avail")
 
     df_orig["subject_id"] = get_public_sub_id(["lhab_" + str(s) for s in df_orig["private_subject_id"]], s_id_lut)
     if df_orig.subject_id.isnull().any():
@@ -17,37 +48,55 @@ def export_behav_with_new_id(orig_file, s_id_lut, out_dir):
         raise Exception("something went wrong with subject_id transformation")
     df_orig.drop("private_subject_id", axis=1, inplace=True)
 
-    score_names = list(set(["_".join(s.split("_")[:-1]) for s in score_cols]))
-    if len(score_names) > 1:
-        raise NotImplementedError()
-
     df_long = pd.melt(df_orig, id_vars=["subject_id"],
                       value_vars=score_cols,
-                      var_name='session_id', value_name=score_names[0])
-    rename_ses = {}
-    for s in score_cols:
-        rename_ses[s] = s.split("_")[-1]
-
-    df_long["session_id"].replace(rename_ses, inplace=True)
+                      var_name='test_session_id', value_name="value")  # score_names[0])
+    df_long["session_id"] = df_long["test_session_id"].apply(lambda s: s.split("_")[-1])
+    df_long["variable"] = df_long["test_session_id"].apply(lambda s: "_".join(s.split("_")[0:-1]))
+    df_long.drop("test_session_id", axis=1, inplace=True)
     df_long.dropna(inplace=True)
-    df_long.to_csv(os.path.join(out_dir, os.path.basename(orig_file).split(".")[0] + ".tsv"), index=None, sep="\t")
+    df_wide = long_to_wide(df_long, ["subject_id", "session_id"], ["variable"], ["value"])
+    df_wide.rename(columns=lambda c: c.split("test_score_")[-1], inplace=True)
+    return df_long, df_wide
+
+
 
 
 s_id_lut = "/Volumes/lhab_raw/01_RAW/00_PRIVATE_sub_lists/new_sub_id_lut.tsv"
-file_list = [
-    "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/03_ready2use/03_processingspeed/lhab_tmta_tp1_tp2_tp3_23052017.xlsx",
-    "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/01_ready4check/06_digsymbol_tp1_tp2_tp3.xlsx",
-    "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/01_ready4check/12_lps14_tp1_tp2_tp3.xlsx",
-    "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/01_ready4check/07_ipt_tp1_tp2_tp3.xlsx"
-    ]
-out_dir = "/Users/franzliem/Dropbox/OHBM2017/analysis/lhab/prepare_behav"
 
-for orig_file in file_list:
-    export_behav_with_new_id(orig_file, s_id_lut, out_dir)
+in_dir = "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/04_ready2use/"
+out_dir = "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/05_ready2_use_newIDs/"
 
-os.chdir(out_dir)
-df = pd.DataFrame([], columns=["subject_id", "session_id"])
-for f in sorted(glob("*.tsv")):
-    df_ = pd.read_csv(f, sep="\t")
-    df = df.merge(df_, how="outer", on=["subject_id", "session_id"])
-df.to_csv("speed.tsv", index=None, sep="\t")
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+os.chdir(in_dir)
+
+domains = sorted(glob("*"))
+
+for d in domains:
+    df_long = pd.DataFrame([], columns=["subject_id", "session_id", "file"])
+    df_wide = pd.DataFrame([], columns=["subject_id", "session_id"])
+
+    os.chdir(os.path.join(in_dir, d))
+    xl_list = sorted(glob("*.xlsx"))
+    xl_list = [x for x in xl_list if "metadata" not in x]
+
+    for orig_file in xl_list:
+        print(orig_file)
+        df_long_, df_wide_ = export_behav_with_new_id(os.path.join(in_dir, d, orig_file), s_id_lut)
+        df_long_["file"] = orig_file
+
+        df_long = df_long.append(df_long_)
+        df_wide = df_wide.merge(df_wide_, how="outer", on=["subject_id", "session_id"])
+
+
+    c = df_long.columns.drop(["subject_id", "session_id", "file"]).tolist()
+    df_long = df_long[["subject_id", "session_id"] + c + ["file"]]
+
+    out_file = os.path.join(out_dir, d + "_long.tsv")
+    df_long.to_csv(out_file, index=None, sep="\t")
+
+    out_file = os.path.join(out_dir, d + "_wide.tsv")
+    df_wide.to_csv(out_file, index=None, sep="\t")
