1f75f5f5873e34b1e9cf966a8437205091a2a70e
fliem <franz.liem@gmail.com>
2017-11-02 16:24:21 +0100

    first working missing export

--- lhab_pipelines/behav/behav_utils.py
+++ lhab_pipelines/behav/behav_utils.py
@@ -6,7 +6,8 @@ from lhab_pipelines.nii_conversion.utils import get_public_sub_id
 
 def long_to_wide(long, index, columns, values, nonumeric=False):
     if nonumeric:
-        wide = long.pivot_table(index=index, columns=columns, values=values, aggfunc=lambda x: ', '.join(x.unique()))
+        wide = long.pivot_table(index=index, columns=columns, values=values, aggfunc="first")  # aggfunc=lambda x: ',
+        # '.join(x.unique()))
     else:
         wide = long.pivot_table(index=index, columns=columns, values=values)
     l = wide.columns.labels
@@ -28,40 +29,123 @@ def long_to_wide(long, index, columns, values, nonumeric=False):
     return wide
 
 
-def export_behav_with_new_id(orig_file, s_id_lut):
+def load_data_excel(orig_file, s_id_lut, tp_string_last=True):
+    """
+    tp_string_last: if True: expects columns of type: <name>_tp1
+                    if False: expects columns of type: tp1_<name> (e.g., missing files)
+
+    """
+
     df_orig = pd.read_excel(orig_file, na_values=["NA01", "NA02", "NA03", "NA04", "NA4", "NA1", "NA2", "TL", "X", 888,
                                                   999])
     # na_values seems not to catch numbers consistently
     df_orig.replace({888: np.nan, 999: np.nan}, inplace=True)
 
-    # fixme remove na counts in last line
-    df_orig.dropna(subset=["vp_code"], inplace=True)
-    df_orig.rename(columns={"vp_code": "private_subject_id"}, inplace=True)
-    # df_orig["private_subject_id"].replace({"vcp4 ": "vcp4"}, inplace=True)
-    df_orig["private_subject_id"] = df_orig["private_subject_id"].str.strip()
-    score_cols = df_orig.columns.drop(['private_subject_id']).tolist()
-    if "tp_avail" in score_cols:
-        score_cols = score_cols.drop("tp_avail")
+    if (df_orig.columns.tolist() == ['no missings']) & (df_orig.shape == (0, 1)):  # metadata file with no missings
+        df_orig = None
+        df_long = None
+    else:
+        df_orig.dropna(subset=["vp_code"], inplace=True)
+        df_orig.rename(columns={"vp_code": "private_subject_id"}, inplace=True)
 
-    df_orig["subject_id"] = get_public_sub_id(["lhab_" + str(s) for s in df_orig["private_subject_id"]], s_id_lut)
-    if df_orig.subject_id.isnull().any():
-        df_orig.to_clipboard()
-        raise Exception("something went wrong with subject_id transformation")
-    df_orig.drop("private_subject_id", axis=1, inplace=True)
+        # df_orig["private_subject_id"].replace({"vcp4 ": "vcp4"}, inplace=True)
+        df_orig["private_subject_id"] = df_orig["private_subject_id"].str.strip()
+        score_cols = df_orig.columns.drop(['private_subject_id']).tolist()
+        if "tp_avail" in score_cols:
+            score_cols = score_cols.drop("tp_avail")
 
-    df_long = pd.melt(df_orig, id_vars=["subject_id"],
-                      value_vars=score_cols,
-                      var_name='test_session_id', value_name="score_value")  # score_names[0])
-    df_long["session_id"] = df_long["test_session_id"].apply(lambda s: s.split("_")[-1])
-    df_long["score_name"] = df_long["test_session_id"].apply(lambda s: "_".join(s.split("_")[0:-1]))
-    df_long.drop("test_session_id", axis=1, inplace=True)
-    df_long.dropna(inplace=True)
-    df_wide = long_to_wide(df_long, ["subject_id", "session_id"], ["score_name"], ["score_value"])
-    df_wide.rename(columns=lambda c: c.split("test_score_")[-1], inplace=True)
+        df_orig["subject_id"] = get_public_sub_id(["lhab_" + str(s) for s in df_orig["private_subject_id"]], s_id_lut)
+        if df_orig.subject_id.isnull().any():
+            df_orig.to_clipboard()
+            raise Exception("something went wrong with subject_id transformation")
+        df_orig.drop("private_subject_id", axis=1, inplace=True)
 
-    df_wide.sort_values(by=["subject_id", "session_id"], inplace=True)
-    df_long.sort_values(by=["subject_id", "session_id"], inplace=True)
+        df_long = pd.melt(df_orig, id_vars=["subject_id"],
+                          value_vars=score_cols,
+                          var_name='test_session_id', value_name="score_value")  # score_names[0])
 
-    df_long["conversion_date"] = pd.datetime.now().date().isoformat()
-    df_wide["conversion_date"] = pd.datetime.now().date().isoformat()
-    return df_long, df_wide
+        if tp_string_last:
+            df_long["session_id"] = df_long["test_session_id"].apply(lambda s: s.split("_")[-1])
+            df_long["score_name"] = df_long["test_session_id"].apply(lambda s: "_".join(s.split("_")[0:-1]))
+        else:
+            df_long["session_id"] = df_long["test_session_id"].apply(lambda s: s.split("_")[0])
+            df_long["score_name"] = df_long["test_session_id"].apply(lambda s: "_".join(s.split("_")[1:]))
+        df_long.drop("test_session_id", axis=1, inplace=True)
+        df_long.dropna(inplace=True)
+
+    return df_orig, df_long
+
+
+def prepare_missing_df(df_meta_long):
+    "Takes metadata df, retains only scores that are missing and replaces them with text info"
+    lookup_dict = {
+        2: "medical_reasons",
+        3: "instructions_not_understood",
+        4: "test_termination_by_subject",
+        5: "environmental_circumstance",
+        6: "test_supervisor_error",
+        7: "unknown_reasons",
+    }
+
+    missing_info = df_meta_long[df_meta_long.score_name == "missing"]
+    missing_details = df_meta_long[df_meta_long.score_name == "missing_text"]
+    missing = pd.merge(missing_info, missing_details, how="outer", on=["subject_id", "session_id"],
+                       suffixes=("_info", "_details"))
+    missing = missing[missing.score_value_info > 0]
+
+    # disambiguate subjects with whole time point not available ("1") into
+    #   - entire timepoint missing and
+    #   - entire timepoint missing because not invited (tp4) and
+    #   -dropout
+    missing["info"] = np.nan
+    missing.replace({"score_value_details":{"missing tp": "missing_tp"}}, inplace=True)
+
+    missing.loc[missing.score_value_info == 1, "info"] = missing.score_value_details
+    missing.loc[((missing.score_value_info == 1) & (missing.session_id == "tp4")), "info"] = "missing_tp_not_invited"
+
+    # for all other cases take numeric info and replace with text
+    missing.loc[missing.score_value_info > 1, "info"] = missing.score_value_info
+    missing.replace({"info": lookup_dict}, inplace=True)
+
+    missing = missing[["subject_id", "session_id", "info"]]
+    missing["data_missing"] = True
+    missing.sort_values(by=["subject_id", "session_id"], inplace=True)
+    return missing
+
+
+def set_invalid_values_to_nan(df_long, df_meta_long):
+    missing_full_info = prepare_missing_df(df_meta_long)
+
+    # remove scores that are there but are invalid
+    missing = missing_full_info[["subject_id", "session_id", "data_missing"]].copy()
+    df_long_clean = pd.merge(df_long, missing, on=["subject_id", "session_id"], how="left")
+    df_long_clean.loc[df_long_clean["data_missing"] == True, "score_value"] = np.nan
+
+    n = df_long_clean.loc[df_long_clean["data_missing"] == True].shape[0]
+    if n > 0:
+        print("removed {} invalid values".format(n))
+    df_long_clean.drop(["data_missing"], axis=1, inplace=True)
+    df_long_clean.dropna(inplace=True)
+    return missing_full_info, df_long_clean
+
+
+def export_behav_with_new_id(orig_file, metadata_file, s_id_lut):
+    df_orig, df_long = load_data_excel(orig_file, s_id_lut)
+    df_meta_orig, df_meta_long = load_data_excel(metadata_file, s_id_lut, tp_string_last=False)
+
+    if not df_meta_long is None:
+        # remove values that should be missing but are not
+        missing_full_info, df_long_clean = set_invalid_values_to_nan(df_long, df_meta_long)
+    else:  # if no missing
+        missing_full_info = pd.DataFrame([])
+        df_long_clean = df_long
+
+    df_wide_clean = long_to_wide(df_long_clean, ["subject_id", "session_id"], ["score_name"], ["score_value"])
+    df_wide_clean.rename(columns=lambda c: c.split("test_score_")[-1], inplace=True)
+
+    df_wide_clean.sort_values(by=["subject_id", "session_id"], inplace=True)
+    df_long_clean.sort_values(by=["subject_id", "session_id"], inplace=True)
+
+    df_long_clean["conversion_date"] = pd.datetime.now().date().isoformat()
+    df_wide_clean["conversion_date"] = pd.datetime.now().date().isoformat()
+    return df_long_clean, df_wide_clean, missing_full_info
--- scripts/behav/export_behav.py
+++ scripts/behav/export_behav.py
@@ -6,26 +6,28 @@ import pandas as pd
 from lhab_pipelines.behav.behav_utils import export_behav_with_new_id
 
 s_id_lut = "/Volumes/lhab_raw/01_RAW/00_PRIVATE_sub_lists/new_sub_id_lut.tsv"
-
-in_dir = "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/04_ready2use/"
+# fixme
+in_dir = "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/04_ready2use_test/"
 # fixme
 # out_dir = "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/05_ready2_use_newIDs/"
 out_dir = "/Volumes/lhab_public/03_Data/99_CleaningT1T2T3/01_Cognition/textfiles/05_ready2_use_newIDs_test/"
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+data_out_dir = os.path.join(out_dir, "data")
+missing_out_dir = os.path.join(out_dir, "missing_info")
+
+for p in [out_dir, data_out_dir, missing_out_dir]:
+    if not os.path.exists(p):
+        os.makedirs(p)
 
 os.chdir(in_dir)
 
 domains = sorted(glob("*"))
 
-# # fimxe remove
-# print("NOT CONVERTING SF12")
-# domains.remove("00_sf12")
 
 for d in domains:
     df_long = pd.DataFrame([], columns=["subject_id", "session_id", "conversion_date", "file"])
     df_wide = pd.DataFrame([], columns=["subject_id", "session_id", "conversion_date"])
+    missing_info = pd.DataFrame([], columns=["subject_id", "session_id", "conversion_date", "file"])
 
     os.chdir(os.path.join(in_dir, d))
     xl_list = sorted(glob("*.xlsx"))
@@ -33,20 +35,39 @@ for d in domains:
 
     for orig_file in xl_list:
         print(orig_file)
-        df_long_, df_wide_ = export_behav_with_new_id(os.path.join(in_dir, d, orig_file), s_id_lut)
+        data_file = os.path.join(in_dir, d, orig_file)
+
+        metadata_str = "_".join(orig_file.split("_")[:2]) + "*" + "_metadata.xlsx"
+        g = glob(metadata_str)
+        if len(g) > 1:
+            raise Exception("More than one meta data file found: {}".format(g))
+        elif len(g) == 0:
+            raise Exception("No meta data file found: {}".format(metadata_str))
+        else:
+            metadata_file = g[0]
+        data_file_path = os.path.join(in_dir, d, metadata_file)
+
+        df_long_, df_wide_, missing_info_ = export_behav_with_new_id(data_file,data_file_path, s_id_lut)
         df_long_["file"] = orig_file
+        missing_info_["file"] = metadata_file
 
         df_long = df_long.append(df_long_)
         df_wide = df_wide.merge(df_wide_, how="outer", on=["subject_id", "session_id", "conversion_date"])
+        missing_info = missing_info.append(missing_info_)
 
     # sort columns
     c = df_long.columns.drop(["subject_id", "session_id", "file", "conversion_date"]).tolist()
     df_long = df_long[["subject_id", "session_id"] + c + ["file", "conversion_date"]]
     c = df_wide.columns.drop(["subject_id", "session_id", "conversion_date"]).tolist()
     df_wide = df_wide[["subject_id", "session_id"] + c + ["conversion_date"]]
+    c = missing_info.columns.drop(["subject_id", "session_id", "file", "conversion_date"]).tolist()
+    missing_info = missing_info[["subject_id", "session_id"] + c + ["file", "conversion_date"]]
 
-    out_file = os.path.join(out_dir, d + "_long.tsv")
+    out_file = os.path.join(data_out_dir, d + "_long.tsv")
     df_long.to_csv(out_file, index=None, sep="\t")
 
-    out_file = os.path.join(out_dir, d + "_wide.tsv")
+    out_file = os.path.join(data_out_dir, d + "_wide.tsv")
     df_wide.to_csv(out_file, index=None, sep="\t")
+
+    out_file = os.path.join(missing_out_dir, d + "_missing_info.tsv")
+    missing_info.to_csv(out_file, index=None, sep="\t")
