f3c08b1e100f21f27cdcda1b568fe99588d815c0
fliem <franz.liem@gmail.com>
2019-12-03 13:50:09 +0100

    ud missing reasons

--- lhab_pipelines/behav/behav_utils.py
+++ lhab_pipelines/behav/behav_utils.py
@@ -93,7 +93,8 @@ def prepare_missing_df(df_meta_long):
         5: "environmental_circumstance",
         6: "test_supervisor_error",
         7: "unknown_reasons",
-        8: "Time reasons"
+        8: "Time reasons",
+        99: "Unknown Reasons"
     }
 
     missing_info = df_meta_long[df_meta_long.score_name == "missing"]
