e44dbb9d127af0aa23723e0e023cfee3413770ed
fliem <franz.liem@gmail.com>
2020-01-06 11:04:15 +0100

    first tp1style test

---
+++ scripts/nii_conversion/phantom_tests/run_check_nii_conversion_phantom_tp1style.py
@@ -0,0 +1,96 @@
+"""
+CI Test for LHAB conversion pipeline (par to nifti).
+- converts example data set (phantom)
+- tests if files nifti and json files are there
+- tests if sensitive information is deleted
+- runs bids validator
+
+- does NOT check dwi data conversion
+
+- docker run --rm -ti -v ${HOME}/outputs:/data/out fliem/${CIRCLE_PROJECT_REPONAME,,} python /code/lhab_pipelines/scripts/nii_conversion/phantom_tests/run_check_nii_conversion_phantom_tp1style.py /code/lhab_pipelines/scripts/nii_conversion/phantom_tests/01_RAW /data/out participant --ds_version phantomas
+"""
+
+import os, argparse
+from pathlib import Path
+
+from lhab_pipelines.utils import read_tsv
+from os.path import join as oj
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='LHAB raw to nifti conversion. \nBIDS-Apps compatiple arguments.'
+                                                 "\nexample:\n python run_nii_conversion.py /data/raw /data/ participant "
+                                                 "--no-public_output --participant_label lhab_1c")
+    parser.add_argument('output_base_dir', help='The directory where the output files should be stored.')
+    parser.add_argument('--participant_label', help='The label of the participant that should be analyzed.'
+                                                    'For the conversion wf this should be given as lhab_1234',
+                        nargs="+")
+    parser.add_argument('--subject_list_file', help='File with subject_ids')
+    parser.add_argument('--info_out_dir', help='The directory where the misc info files are written to', required=True)
+    parser.add_argument('--no-public_output',
+                        help="Don't create public output.\nIf public_output: strips all info about original "
+                             "subject_id, file, date \nDefault: use public_output",
+                        default=True, dest="public_output", action='store_false')
+    parser.add_argument('--no-use_new_ids', help="Don't use new subject ids. "
+                                                 "\nDefault: Use new ids from mapping file",
+                        default=True, dest="use_new_ids", action='store_false')
+    parser.add_argument('--ds_version', help="Data set version (is added to output path)", default="dev")
+
+    args = parser.parse_args()
+
+    ###
+    if args.participant_label:
+        old_sub_id_list = [s.strip() for s in args.participant_label]
+    else:
+        old_sub_id_list = read_tsv(args.subject_list_file, no_header=True)[0].tolist()
+
+    ## checks
+    private_str = "_PRIVATE" if not (args.public_output and args.use_new_ids) else ""
+    output_dir = Path(args.output_base_dir) / f"LHAB_{args.ds_version}{private_str}" / "sourcedata"
+
+    #
+    print("X" * 20 + "\nTesting if correct files are there and others are correctly deleted...")
+
+    shouldbe_there = [
+        oj(output_dir, "sub-lhabX9999/ses-tp1/anat/sub-lhabX9999_ses-tp1_run-1_T1w.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp3/anat/sub-lhabX9999_ses-tp3_run-1_T1w.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/anat/sub-lhabX9999_ses-tp1_run-1_T1w.json"),
+        oj(output_dir, "sub-lhabX9999/ses-tp3/anat/sub-lhabX9999_ses-tp3_run-1_T1w.json"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/anat/sub-lhabX9999_ses-tp1_acq-2D_run-1_FLAIR.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/anat/sub-lhabX9999_ses-tp1_acq-3D_run-1_FLAIR.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp3/func/sub-lhabX9999_ses-tp3_task-rest_run-1_bold.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp3/fmap/sub-lhabX9999_ses-tp3_acq-pa_run-1_bold.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/dwi/sub-lhabX9999_ses-tp1_acq-ap_run-1_dwi.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/dwi/sub-lhabX9999_ses-tp1_acq-ap_run-1_dwi.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/dwi/sub-lhabX9999_ses-tp1_acq-ap_run-1_dwi.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/dwi/sub-lhabX9999_ses-tp1_acq-ap_run-1_dwi.json"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/dwi/sub-lhabX9999_ses-tp1_acq-ap_run-1_dwi.bval"),
+        oj(output_dir, "sub-lhabX9999/ses-tp1/dwi/sub-lhabX9999_ses-tp1_acq-ap_run-1_dwi.bvec"),
+        oj(output_dir, "sub-lhabX9999/ses-tp3/fmap/sub-lhabX9999_ses-tp3_acq-ap_run-1_dwi.nii.gz"),
+        oj(output_dir, "sub-lhabX9999/ses-tp3/fmap/sub-lhabX9999_ses-tp3_acq-pa_run-1_dwi.nii.gz"),
+
+        oj(args.info_out_dir, "PRIVATE/acq_time_PRIVATE/sub-lhabX9999_ses-tp1_acq_times.tsv"),
+        oj(args.info_out_dir, "PRIVATE/parrec_mapping_PRIVATE/sub-lhabX9999_ses-tp1_par2nii_mapping.tsv"),
+        oj(args.info_out_dir, "PRIVATE/unconverted_files/sub-lhabX9999_ses-tp1_unconverted.tsv"),
+    ]
+
+    for f in shouldbe_there:
+        if not os.path.exists(f):
+            raise FileNotFoundError("A file that the test should produce was missing: %s" % f)
+
+    # check defacing is on
+    f = oj(output_dir, "sub-lhabX9999/ses-tp1/anat/sub-lhabX9999_ses-tp1_run-1_T1w.json")
+    with open(f) as fi:
+        j = json.load(fi)
+    if not j["Defaced"]:
+        raise Exception("Defacing seems to be turned off. Exit. %s" % f)
+
+    if args.public_output:
+        shouldnotbe_there = [
+            oj(output_dir, "sub-lhabX9999/ses-tp1/anat/sub-lhabX9999_ses-tp1_run-1_T1w.txt"),
+            oj(output_dir, "sub-lhabX9999/par2nii_mapping.txt"),
+        ]
+        for f in shouldnotbe_there:
+            if os.path.exists(f):
+                raise FileExistsError("A file that the test should NOT produce was found: %s" % f)
+    print("Everthing seems to be fine!")
--- scripts/nii_conversion/run_nii_conversion.py
+++ scripts/nii_conversion/run_nii_conversion.py
@@ -11,30 +11,30 @@
 |       `-- lhab_xxxx_t2_raw
 |           |-- lhab_xxxx_2dflair_T2.par
 |           |-- lhab_xxxx_2dflair_T2.rec
-...
-
 """
+
 import os, argparse
-from lhab_pipelines.utils import read_tsv, add_info_to_json
-from lhab_pipelines.nii_conversion.conversion import submit_single_subject
-import datetime as dt
-import numpy as np
+from pathlib import Path
+
+from lhab_pipelines.nii_conversion.conversion import run_conversion
+from lhab_pipelines.utils import read_tsv
+from lhab_pipelines.nii_conversion.config import get_info_list_v2, dataset_description_v2
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='LHAB raw to nifti conversion. \nBIDS-Apps compatiple arguments.'
                                                  "\nexample:\n python run_nii_conversion.py /data/raw /data/ participant "
                                                  "--no-public_output --participant_label lhab_1c")
-    parser.add_argument('raw_dir', help='The directory with the RAW input dataset.'
-                                        '\n original: bids_dir')
-    parser.add_argument('output_base_dir', help='The directory where the output files '
-                                                'should be stored.'
-                                                '\n original: output_dir')
+    parser.add_argument('raw_dir', help='The directory with the RAW input dataset.')
+    parser.add_argument('output_base_dir', help='The directory where the output files should be stored.')
     parser.add_argument('analysis_level', help='Level of the analysis that will be performed. ',
-                        choices=['participant'])
+                        choices=['participant', 'group'])
     parser.add_argument('--info_out_dir', help='The directory where the misc info files are written to', required=True)
     parser.add_argument('--participant_label', help='The label of the participant that should be analyzed.'
                                                     'For the conversion wf this should be given as lhab_1234',
                         nargs="+")
+    parser.add_argument('--session_label', help='Old style session labels: T1, T2...',
+                        nargs="+", default=['T1', 'T2', 'T3', 'T4', 'T5', 'T6'])
+    parser.add_argument('--subject_list_file', help='File with subject_ids')
     parser.add_argument('--no-public_output',
                         help="Don't create public output.\nIf public_output: strips all info about original "
                              "subject_id, file, date \nDefault: use public_output",
@@ -43,120 +43,35 @@ if __name__ == "__main__":
                                                  "\nDefault: Use new ids from mapping file",
                         default=True, dest="use_new_ids", action='store_false')
     parser.add_argument('--ds_version', help="Data set version (is added to output path)", default="dev")
+    parser.add_argument('--new_id_lut_file', help="old to new ids")
+    parser.add_argument('--bvecs_from_scanner_file', help="")
+    parser.add_argument('--tp6_raw_lut', help="")
+    parser.add_argument('--dry_run', help="Don't convert, just collect information", default=False,
+                        action='store_true')
+    parser.add_argument('--demo_file', help="file with sex and dob")
 
     args = parser.parse_args()
 
-    raw_dir = args.raw_dir
-
-    # privacy settings
-    public_output = args.public_output
-    use_new_ids = args.use_new_ids
-    if not (public_output and use_new_ids):
-        private_str = "_PRIVATE"
-    else:
-        private_str = ""
-
-    output_dir = os.path.join(args.output_base_dir, "LHAB_" + args.ds_version + private_str, "sourcedata")
+    do_deface = True if args.public_output else False
+    info_list_v2 = get_info_list_v2(do_deface)
 
     ###
     if args.participant_label:
         old_sub_id_list = [s.strip() for s in args.participant_label]
-        exclude_sub_id_list = "none"
     else:
-        # SUBJECT ID STUFF
-        all_sub_file = os.path.join(raw_dir, "00_PRIVATE_sub_lists/lhab_all_subjects.tsv")
-        old_sub_id_list = read_tsv(all_sub_file)["subject_id"].tolist()
+        old_sub_id_list = read_tsv(args.subject_list_file, no_header=True)[0].tolist()
 
-    ###
-    ###
-    ses_id_list = ["T1", "T2", "T3", "T4", "T5"]
-    in_ses_folder = "01_noIF"
-    bvecs_from_scanner_file = os.path.join(raw_dir, "00_bvecs/bvecs.fromscanner")
-    new_id_lut_file = os.path.join(raw_dir, "00_PRIVATE_sub_lists/new_sub_id_lut.tsv")
+    run_conversion(args.raw_dir, args.output_base_dir, args.analysis_level, args.info_out_dir, old_sub_id_list,
+                   args.session_label, args.public_output, args.use_new_ids, args.ds_version, info_list_v2,
+                   dataset_description_v2, args.new_id_lut_file, args.bvecs_from_scanner_file, args.tp6_raw_lut,
+                   args.dry_run, args.demo_file)
 
-    # LAS Orientation
-    # i : RL
-    # i-: LR
-    # j : PA
-    # j-: AP
-    # k : IS
-    # k-: SI
-    general_info = {"MagneticFieldStrength": 3.0, "ManufacturersModelName": "Philips Ingenia"}
-    sense_info = {"ParallelAcquisitionTechnique": "SENSE", "ParallelReductionFactorInPlane": 2}
+    # bids validator
+    private_str = "_PRIVATE" if not (args.public_output and args.use_new_ids) else ""
+    output_dir = Path(args.output_base_dir) / f"LHAB_{args.ds_version}{private_str}" / "sourcedata"
 
-    # TR=2sec, 43 slices,  # ascending sliceorder
-    rs_info = {"SliceEncodingDirection": "k", "SliceTiming": np.arange(0, 2.0, 2. / 43)}
-
-    info_list = [
-        # anatomical
-        {"bids_name": "T1w", "bids_modality": "anat", "search_str": "_t1w_", "deface": True,
-         "add_info": {**general_info}},
-        # flair
-        {"bids_name": "FLAIR", "bids_modality": "anat", "search_str": "_2dflair_", "acq": "2D", "deface": True,
-         "add_info": {**general_info}},
-        {"bids_name": "FLAIR", "bids_modality": "anat", "search_str": "_3dflair_", "acq": "3D", "deface": True,
-         "add_info": {**general_info}},
-
-        # dwi
-        {"bids_name": "dwi", "bids_modality": "dwi", "search_str": "_dti_T", "only_use_last": True, "acq": "ap",
-         "add_info": {**general_info, **sense_info, "PhaseEncodingDirection": "j-"}},
-
-        # func
-        {"bids_name": "bold", "bids_modality": "func", "search_str": "_fmri_T", "task": "rest", "physio": True,
-         "add_info": {**general_info, **sense_info, **rs_info, "PhaseEncodingDirection": "j-"}},
-
-        # fieldmaps
-        {"bids_name": "bold", "bids_modality": "fmap", "search_str": "_fmri_pa_T", "acq": "pa",
-         "add_info": {**general_info, **sense_info, **rs_info, "PhaseEncodingDirection": "j"}},
-        {"bids_name": "dwi", "bids_modality": "fmap", "search_str": "_dti_pa_T", "acq": "pa",
-         "add_info": {**general_info, **sense_info, "PhaseEncodingDirection": "j"}},
-        {"bids_name": "dwi", "bids_modality": "fmap", "search_str": "_dti_ap_T", "acq": "ap",
-         "add_info": {**general_info, **sense_info, "PhaseEncodingDirection": "j-"}}
-    ]
-
-    #
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    info_file = os.path.join(output_dir, "..", "info.txt")
-    s = "\n%s" % dt.datetime.now()
-    s += "\npublic_output: %s" % public_output
-    s += "\nuse_new_ids: %s" % use_new_ids
-    s += "\ninfo_list_v2: %s" % info_list
-    if not os.path.exists(info_file):
-        with open(info_file, "w") as fi:
-            fi.write(s)
-
-    ds_desc_file = os.path.join(output_dir, "dataset_description_v2.json")
-    if not os.path.exists(ds_desc_file):
-        description = {"Name": "LHAB longitudinal healthy aging brain study",
-                       "BIDSVersion": "1.0.1",
-                       # "License": "XXXXXX what license is this dataset distributed under? The use of license name "
-                       #            "abbreviations is suggested for specifying a license. A list of common licenses"
-                       #            " with suggested abbreviations can be found in appendix III.",
-                       # "Authors": "XXXXXX List of individuals who contributed to the creation/curation of the dataset",
-                       # "Acknowledgements": "XXXXXX who should be acknowledge in helping to collect the data",
-                       # "HowToAcknowledge": "XXXXXX Instructions how researchers using this dataset should "
-                       #                     "acknowledge the original authors. This "
-                       #                     "field can also be used to define a publication that should be cited in publications that use "
-                       #                     "the dataset",
-                       "Funding": "Velux Stiftung (project No. 369)",
-                       "DataSetVersion": args.ds_version}
-        add_info_to_json(ds_desc_file, description, create_new=True)
-
-    info_out_dir = None
-    for old_subject_id in old_sub_id_list:
-        submit_single_subject(old_subject_id,
-                              ses_id_list,
-                              raw_dir,
-                              in_ses_folder,
-                              output_dir,
-                              info_list,
-                              args.info_out_dir,
-                              bvecs_from_scanner_file=bvecs_from_scanner_file,
-                              public_output=public_output,
-                              use_new_ids=use_new_ids,
-                              new_id_lut_file=new_id_lut_file)
+    print("X" * 20 + "\nRuning BIDS validator")
+    os.system("bids-validator %s" % output_dir)
 
     print("\n\n\n\nDONE.\nConverted %d subjects." % len(old_sub_id_list))
     print(old_sub_id_list)
